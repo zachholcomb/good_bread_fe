35283a8657b7a4f8082d61fc4df88f51fad1dc91
Zach Holcomb <zachholcombmusic@gmail.com>
2020-09-15 17:31:22 -0600

    Adds new routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,35 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Register from '@/components/Register.vue'
+import Login from '@/components/Login.vue'
+import Home from '@/components/Home.vue'
+import Dashboard from '@/components/Dashboard.vue'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: Home,
+      component: Home
+    },
+    {
+      path: '/register',
+      name: Register,
+      component: Register
+    },
+
+    {
+      path: '/login',
+      name: Login,
+      component: Login
+    },
+    {
+      path: '/dashboard',
+      name: Dashboard,
+      component: Dashboard
     }
   ]
 })
