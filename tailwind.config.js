beb0df80e21244a5c44f00c297311abca5a77751
Zach Holcomb <zachholcombmusic@gmail.com>
2020-09-15 17:29:58 -0600

    Adds tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   purge: [],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: (theme) => ({
+        'bread': "url('/src/assets/bread2.png')"
+      })
+    },
   },
   variants: {},
   plugins: [],
