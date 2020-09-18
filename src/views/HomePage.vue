b4c6cef6a2812a455bb7bd90c072a9d9f27429b6
Zach Holcomb <zachholcombmusic@gmail.com>
2020-09-18 11:36:38 -0600

    Renames home to homepage to follow vue convention

---
+++ src/views/HomePage.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <HomeSplash></HomeSplash>
+  </div>
+
+</template>
+<script>
+import HomeSplash from '@/components/HomeSplash.vue'
+
+export default {
+  name: 'HomePage',
+  components: {
+    HomeSplash
+  }
+}
+</script>
