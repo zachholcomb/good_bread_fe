a29849cf9579e2748e55e5adbe39b6d97db8a155
Zach Holcomb <zachholcombmusic@gmail.com>
2020-09-18 11:35:01 -0600

    Reconfigures axios to live in ApiService

--- src/main.js
+++ src/main.js
@@ -1,25 +1,18 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import VueAxios from 'vue-axios'
-import { securedAxiosInstance, plainAxiosInstance } from './api/axios'
+import store from './store'
 import './css/main.css'
+import ApiService from './common/api.service'
 
 Vue.config.productionTip = false
-
-Vue.use(VueAxios, {
-  secured: securedAxiosInstance,
-  plain: plainAxiosInstance
-})
+ApiService.init()
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  securedAxiosInstance,
-  plainAxiosInstance,
+  store,
   components: { App },
   template: '<App/>'
 })
