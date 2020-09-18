05782a378a1fb1b30334d8d663d880ac7a37cc26
Zach Holcomb <zachholcombmusic@gmail.com>
2020-09-18 11:35:15 -0600

    Updates API_URL

--- src/api/axios/index.js
+++ src/api/axios/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_URL = 'http://localhost:3000'
+const API_URL = 'http://localhost:3000/api/v1'
 
 const securedAxiosInstance = axios.create({
   baseURL: API_URL,
