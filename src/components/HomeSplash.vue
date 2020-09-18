e6e8a09e03f1075ca8928b81dc842d83fe387661
Zach Holcomb <zachholcombmusic@gmail.com>
2020-09-18 11:36:51 -0600

    Adds Homesplash component

---
+++ src/components/HomeSplash.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="py-6 bread-bg">
+    <div class="py-20">
+      <h2 class="text-green-600 text-6xl text-center py-4 text-holder">Bakery Counter Now Open!</h2>
+      <p class="text-green-600 text-2xl text-center py-3 text-holder">1515 Madison Street | Denver, CO | 80206</p>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'HomeSplash'
+}
+</script>
