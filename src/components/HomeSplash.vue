285de687f621252ea9a00a0dd076d339ae8b9283
Zach Holcomb <zachholcombmusic@gmail.com>
2020-09-21 22:29:32 -0600

    Updates homesplash

--- src/components/HomeSplash.vue
+++ src/components/HomeSplash.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="py-6 bread-bg">
+  <div class="py-20 bread-bg">
     <div class="py-20">
       <h2 class="text-green-600 text-6xl text-center py-4 text-holder">Bakery Counter Now Open!</h2>
       <p class="text-green-600 text-2xl text-center py-3 text-holder">1515 Madison Street | Denver, CO | 80206</p>
