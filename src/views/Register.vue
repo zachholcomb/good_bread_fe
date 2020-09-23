b8850e33efc235834557560db2508a49d27fccb7
Zach Holcomb <zachholcombmusic@gmail.com>
2020-09-23 15:59:23 -0600

    Adds areas for zip, state and city to register

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -8,27 +8,34 @@
           <label for="email" class="label">Email</label>
           <input type="email" v-model="email" class="input" id="email" placeholder="email@example.com">
         </div>
-
         <div class="mb-6">
           <label for="name" class="label">Name</label>
           <input type="name" v-model="name" class="input" id="name" placeholder="Bread Loaf">
         </div>
-
         <div class="mb-6">
           <label for="address" class="label">Address</label>
           <input type="address" v-model="address" class="input" id="address" placeholder="12345 Place Pl.">
         </div>
-
+        <div class="mb-6">
+          <label for="city" class="label">City</label>
+          <input type="city" v-model="city" class="input" id="city" placeholder="Denver">
+        </div>
+        <div class="mb-6">
+          <label for="state" class="label">State</label>
+          <input type="state" v-model="state" class="input" id="state" placeholder="CO">
+        </div>
+        <div class="mb-6">
+          <label for="zip" class="label">Zip</label>
+          <input type="zip" v-model="zip" class="input" id="zip" placeholder="12345 Place Pl.">
+        </div>
         <div class="mb-6">
           <label for="password" class="label">Password</label>
           <input type="password" v-model="password" class="input" id="password">
         </div>
-
         <div class="mb-6">
           <label for="password-confirmation" class="label">Retype Password</label>
           <input type="password-confirmation" v-model="password_confirmation" class="input" id="password-confirmation">
         </div>
-
         <button type="submit" class="button">Register</button>
         <div class="my-4">
           <router-link to="/login" class="link-gray">Log In</router-link>
@@ -48,6 +55,9 @@ export default {
       email: '',
       name: '',
       address: '',
+      city: '',
+      state: '',
+      zip: '',
       password: '',
       password_confirmation: '',
       error: ''
@@ -60,6 +70,9 @@ export default {
           email: this.email,
           name: this.name,
           address: this.address,
+          city: this.city,
+          state: this.state,
+          zip: this.zip,
           password: this.password,
           password_confirmation: this.password_confirmation
         })
