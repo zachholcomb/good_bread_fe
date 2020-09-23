039271d83b257972d33b226a6f1e4a131b4f5703
Zach Holcomb <zachholcombmusic@gmail.com>
2020-09-23 15:57:04 -0600

    Adds order history component

--- src/components/OrderHistory.vue
+++ src/components/OrderHistory.vue
@@ -1,10 +1,28 @@
 <template>
   <div class="text-center">
     <h3>Order History</h3>
+    <div v-for="order in userOrders" v-bind:key="order.id">
+      {{ console.log(userOrders) }}
+      {{ console.log("OrderHISTORY", order) }}
+    </div>
   </div>
 </template>
 <script>
+import { mapGetters } from 'vuex'
+import { USER_ORDERS } from '@/store/actions.type'
+
 export default {
-  name: 'OrderHistory'
+  name: 'OrderHistory',
+  beforeCreate () {
+    const params = { 
+      token: this.$store.getters['getToken'], 
+      userId: this.$store.getters['getUser'].id 
+    }
+    this.$store
+      .dispatch(USER_ORDERS, params)
+  },
+  computed: {
+    ...mapGetters(['userOrders'])
+  }
 }
 </script>
