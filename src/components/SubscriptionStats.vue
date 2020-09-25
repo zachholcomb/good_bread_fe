412b23e779815bed5f0633b8187c43d8e9865d75
Zach Holcomb <zachholcombmusic@gmail.com>
2020-09-25 14:39:27 -0600

    Updates styling for subscription stats

--- src/components/SubscriptionStats.vue
+++ src/components/SubscriptionStats.vue
@@ -1,11 +1,13 @@
 <template>
-  <div class="max-w-sm m-auto flex">
+  <div class="max-w-sm m-auto flex bg-white">
     <div class="border p-10 border-gray-200 shadow rounded text-gray-700">
       <h3 class="text-4xl mb-8">Your Subscription</h3>
-      <h3>{{ userSub.subscription_type }}</h3>
-      <h3>{{ userSub.delivery_day }}</h3>
-      <div v-for="shipment in userShipments" v-bind:key="shipment.id">
-        <p>{{ shipment.attributes.status }} {{ shipment.attributes.delivery_date }}</p>
+      <h3>Type: {{ userSub.subscription_type }}</h3>
+      <h3>Ship Date: {{ userSub.delivery_day }}</h3>
+      <div class="divide-y divide-gray-400 w-48 m-auto text-center">
+        <div v-for="shipment in userShipments" v-bind:key="shipment.id" class="max-w-xs m-auto pt-2">
+          <p>{{ shipment.attributes.status }} {{ shipment.attributes.delivery_date }}</p>
+        </div>
       </div>
     </div>
   </div>
