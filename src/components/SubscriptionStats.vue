e918f1433f9bd6ead8a50a38b0af462225359554
Zach Holcomb <zachholcombmusic@gmail.com>
2020-10-01 12:00:19 -0600

    Adds Subscription update method and html for update modal to subscription stats

--- src/components/SubscriptionStats.vue
+++ src/components/SubscriptionStats.vue
@@ -1,5 +1,27 @@
 <template>
   <div class="max-w-sm m-auto flex bg-white">
+    <UpdateModal v-model="modalOpen">
+      <div class="text-gray-700 center-text">
+        <h3 class="text-4xl">Update Shipping Address</h3>
+        <form @submit.prevent="updateSubscription">
+          <!-- <div class="text-red-600" v-if="errors">{{ errors }}</div> -->
+          <div class="mb-6">
+            <label for="status" class="label">Subscription Type</label>
+            <input type="name" v-model="userSub.subscription_type" class="input" id="name" placeholder="Bread Loaf">
+          </div>
+          <div class="mb-6">
+            <label for="Ship Day" class="label">Preferred Delivery Date</label>
+            <input type="address" v-model="userSub.delivery_day" class="input" id="address" placeholder="12345 Place Pl.">
+          </div>
+          <button 
+            type="submit"       
+            class="flex-shrink-0 border-transparent border-4 bg-teal-500 hover:bg-teal-800 text-sm py-1 px-20 rounded text-white"
+          >
+            Update
+          </button>
+        </form>
+      </div>
+    </UpdateModal>
     <div class="border p-10 border-gray-200 shadow rounded text-gray-700">
       <h3 class="text-4xl mb-8">Your Subscription</h3>
       <h3>Type: {{ userSub.subscription_type }}</h3>
@@ -9,16 +31,32 @@
           <p>{{ shipment.attributes.status }} {{ shipment.attributes.delivery_date }}</p>
         </div>
       </div>
+      <div class="text-center pt-4">
+        <button 
+          class="flex-shrink-0 border-transparent border-4 bg-teal-500 hover:bg-teal-800 text-sm py-1 px-8 rounded text-white" 
+          @click.prevent="openModal"
+        >
+          Edit
+        </button>
+      </div>
     </div>
   </div>
 </template>
 <script>
-import { USER_SUB } from '@/store/actions.type'
+import { USER_SUB, UPDATE_SUB } from '@/store/actions.type'
 import { mapGetters } from 'vuex'
+import UpdateModal from '@/components/UpdateModal.vue'
 
 export default {
   name: 'SubscriptionStats',
-
+  components: {
+    UpdateModal
+  },
+  data () {
+    return {
+      modalOpen: false
+    }
+  },
   mounted () {
     const params = {
       token: this.$store.getters['getToken'],
@@ -29,6 +67,24 @@ export default {
   },
   computed: {
     ...mapGetters(['userSub', 'userShipments'])
+  },
+  methods: {
+    openModal () {
+      this.modalOpen = true
+    },
+    updateSubscription () {
+      const params = {
+        token: this.$store.getters['getToken'],
+        userId: this.$store.getters['getUser'].id,
+        update_sub: {
+          subscription_type: this.userSub.subscription_type,
+          delivery_day: this.userSub.delivery_day
+        }
+      }
+      this.modalOpen = false
+      this.$store
+        .dispatch(UPDATE_SUB, params)
+    }
   }
 }
 </script>
