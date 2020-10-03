39e9d5555030bb635896ac96ba0b35091230aac0
Zach Holcomb <zachholcombmusic@gmail.com>
2020-10-03 16:19:31 -0600

    Adds shop view

--- src/views/Shop.vue
+++ src/views/Shop.vue
@@ -1,9 +1,26 @@
 <template>
   <div>
+    <TabStore v-model="getItems"></TabStore>
   </div>
 </template>
 <script>
+import TabStore from '@/components/TabStore.vue'
+import { GET_ITEMS } from '@/store/actions.type'
+import { mapGetters } from 'vuex'
+
 export default {
-  name: 'Shop'
+  name: 'Shop',
+  components: {
+    TabStore
+  },
+  created () {
+    if (!this.getItems) {
+      this.$store
+        .dispatch(GET_ITEMS)
+    }
+  },
+  computed: {
+    ...mapGetters(['getItems'])
+  }
 }
 </script>
