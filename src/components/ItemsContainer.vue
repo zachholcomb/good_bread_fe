6ded59e2da573abd09d28f575786ea0f2ad5ef44
Zach Holcomb <zachholcombmusic@gmail.com>
2020-10-03 16:17:59 -0600

    Adds items container and tile

---
+++ src/components/ItemsContainer.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="border-double container bg-gray-200 max-w-6xl m-auto">
+   <slot />
+  </div>
+</template>
+<script>
+export default {
+  name: 'ItemsContainer'
+}
+</script>
