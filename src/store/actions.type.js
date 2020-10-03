5384cf59a28601888c0e5eef322e7b7877090f12
Zach Holcomb <zachholcombmusic@gmail.com>
2020-10-03 16:18:46 -0600

    Adds items actions and mutations

--- src/store/actions.type.js
+++ src/store/actions.type.js
@@ -1,3 +1,4 @@
+// USER ACTIONS
 export const LOGIN = 'login'
 export const LOGOUT = 'logout'
 export const REGISTER = 'register'
@@ -5,3 +6,6 @@ export const UPDATE_USER = 'updateUser'
 export const USER_SUB = 'userSub'
 export const USER_ORDERS = 'userOrders'
 export const UPDATE_SUB = 'updateSub'
+
+// ITEM ACTIONS
+export const GET_ITEMS = 'getItems'
--- src/store/mutations.type.js
+++ src/store/mutations.type.js
@@ -1,5 +1,9 @@
+// USER MUTATIONS
 export const SET_AUTH = 'setUser'
 export const SET_ERROR = 'setError'
 export const PURGE_AUTH = 'purgeAuth'
 export const SET_SUB = 'setSub'
 export const SET_ORDERS = 'setOrders'
+
+// ITEM MUTATIONS
+export const SET_ITEMS = 'setItems'
