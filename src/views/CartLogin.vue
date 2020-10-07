29bb8315bc536110d11f6da083ccd31a45540d00
Zach Holcomb <zachholcombmusic@gmail.com>
2020-10-07 13:36:45 -0600

    Adds cart login page to views

---
+++ src/views/CartLogin.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="max-w-2xl m-auto p-12">
+    <div class="grid grid-cols-2 border border-gray-200 shadow rounded divide-x">
+      <UserCheckout></UserCheckout>
+      <GuestCheckout></GuestCheckout>
+    </div>
+  </div>
+</template>
+<script>
+import GuestCheckout from '@/components/GuestCheckout.vue'
+import UserCheckout from '@/components/UserCheckout.vue'
+
+export default {
+  name: 'CartLogin',
+  components: {
+    GuestCheckout,
+    UserCheckout
+  }
+}
+</script>
