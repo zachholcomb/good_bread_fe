dd5f17e5a3e5ff131502328eb233f903fed675e3
Zach Holcomb <zachholcombmusic@gmail.com>
2020-10-07 13:37:44 -0600

    Adds tab store component for tab navigation in store view

--- src/components/TabStore.vue
+++ src/components/TabStore.vue
@@ -2,51 +2,61 @@
   <div>
     <div class="max-w-sm m-auto pt-5">
       <div class="flex items-center">
-        <button @click='selectTab(1)' 
+        <button @click='selectTab(1)'
                 class="flex-1 bg-gray-200 p-4 font-bold text-gray-700"
                 :class="{ 'bg-teal-500 text-white' : currentTab == 1 }"
         >
           Bread
         </button>
-        <button @click='selectTab(2)' 
+        <button @click='selectTab(2)'
                 class="flex-1 bg-gray-200 p-4 font-bold text-gray-700"
                 :class="{ 'bg-teal-500 text-white' : currentTab == 2 }"
         >
           Pastries
         </button>
-        <button @click='selectTab(3)' 
+        <button @click='selectTab(3)'
                 class="flex-1 bg-gray-200 p-4 font-bold text-gray-700"
                 :class="{ 'bg-teal-500 text-white' : currentTab == 3 }"
         >
-          Donut
+          Donuts
         </button>
-        <button @click='selectTab(4)' 
+        <button @click='selectTab(4)'
                 class="flex-1 bg-gray-200 p-4 font-bold text-gray-700"
                 :class="{ 'bg-teal-500 text-white' : currentTab == 4 }"
         >
           Bagels
         </button>
+        <button @click='selectTab(5)'
+                class="flex-1 bg-gray-200 p-4 font-bold text-gray-700"
+                :class="{ 'bg-teal-500 text-white' : currentTab == 5 }"
+        >
+          All
+        </button>
       </div>
     </div>
-    <div v-if="currentTab ==1">
-      <ItemsContainer>
+    <div v-if="currentTab == 1">
+      <ItemsContainer v-bind:items='this.getBread'>
         Our Bread
-        {{ getItems }}
       </ItemsContainer>
     </div>
-    <div v-if="currentTab ==2">
-      <ItemsContainer>
+    <div v-if="currentTab == 2">
+      <ItemsContainer v-bind:items='this.getPastries'>
         Our Pastries
       </ItemsContainer>
     </div>
-    <div v-if="currentTab ==3">
-      <ItemsContainer>
+    <div v-if="currentTab == 3">
+      <ItemsContainer v-bind:items='this.getDonuts'>
         Try Our Donuts
       </ItemsContainer>
     </div>
-    <div v-if="currentTab ==4">
-      <ItemsContainer>
+    <div v-if="currentTab == 4">
+      <ItemsContainer v-bind:items='this.getBagels'>
         BAGELS!
+      </ItemsContainer>
+    </div>
+    <div v-if="currentTab == 5">
+      <ItemsContainer v-bind:items='this.getItems'>
+        All Our Offerings
       </ItemsContainer>
     </div>
   </div>
@@ -56,10 +66,10 @@ import { mapGetters } from 'vuex'
 import ItemsContainer from '@/components/ItemsContainer.vue'
 
 export default {
-  name: "TabStore",
+  name: 'TabStore',
   data () {
     return {
-      currentTab: 1,
+      currentTab: 1
     }
   },
   methods: {
@@ -68,10 +78,11 @@ export default {
     }
   },
   computed: {
-    ...mapGetters(['getItems'])
+    ...mapGetters(['getItems', 'getBread', 'getPastries', 'getBagels', 'getDonuts'])
   },
   components: {
     ItemsContainer
   }
 }
+console.log(this.getItems)
 </script>
