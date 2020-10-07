4c631e8acc8b3cbf392e4d66e48efb28925ad42e
Zach Holcomb <zachholcombmusic@gmail.com>
2020-10-07 13:42:18 -0600

    Adds getters to separate items in to categories

--- src/store/shop.module.js
+++ src/store/shop.module.js
@@ -3,12 +3,28 @@ import { GET_ITEMS } from './actions.type'
 import { SET_ITEMS, SET_ERROR } from './mutations.type'
 
 const state = {
-  items: null
+  items: null,
+  bread: null,
+  pastries: null,
+  donuts: null,
+  bagels: null
 }
 
 const getters = {
   getItems (state) {
     return state.items
+  },
+  getBread (state) {
+    return state.bread
+  },
+  getPastries (state) {
+    return state.pastries
+  },
+  getDonuts (state) {
+    return state.donuts
+  },
+  getBagels (state) {
+    return state.bagels
   }
 }
 
@@ -29,7 +45,16 @@ const actions = {
 
 const mutations = {
   [SET_ITEMS] (state, data) {
+    const bread = data.data.filter(item => item.attributes.item_type === 'Bread')
+    const pastries = data.data.filter(item => item.attributes.item_type === 'Pastries')
+    const donuts = data.data.filter(item => item.attributes.item_type === 'Donuts')
+    const bagels = data.data.filter(item => item.attributes.item_type === 'Bagels')
+
     state.items = data.data
+    state.bread = bread
+    state.pastries = pastries
+    state.donuts = donuts
+    state.bagels = bagels
   },
   [SET_ERROR] (state, data) {
     state.errors = data
@@ -42,4 +67,3 @@ export default {
   mutations,
   getters
 }
-
