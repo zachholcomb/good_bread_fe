55fa914eaebd8a1dc9cec455ab8402fe572b2f0c
Zach Holcomb <zachholcombmusic@gmail.com>
2020-10-20 16:28:19 -0600

    Update minor styling in profile stats and tab store

--- src/components/TabStore.vue
+++ src/components/TabStore.vue
@@ -1,33 +1,33 @@
 <template>
   <div>
-    <div class="max-w-sm m-auto pt-5">
+    <div class="max-w-sm m-auto pt-8">
       <div class="flex items-center">
         <button @click='selectTab(1)'
-                class="flex-1 bg-gray-200 p-4 font-bold text-gray-700"
+                class="flex-1 bg-gray-200 p-4 font-light text-gray-700 tracking-widest"
                 :class="{ 'bg-teal-200 text-white' : currentTab == 1 }"
         >
           Bread
         </button>
         <button @click='selectTab(2)'
-                class="flex-1 bg-gray-200 p-4 font-bold text-gray-700"
+                class="flex-1 bg-gray-200 p-4 font-light text-gray-700 tracking-widest"
                 :class="{ 'bg-teal-200 text-white' : currentTab == 2 }"
         >
           Pastries
         </button>
         <button @click='selectTab(3)'
-                class="flex-1 bg-gray-200 p-4 font-bold text-gray-700"
+                class="flex-1 bg-gray-200 p-4 font-light text-gray-700 tracking-widest"
                 :class="{ 'bg-teal-200 text-white' : currentTab == 3 }"
         >
           Donuts
         </button>
         <button @click='selectTab(4)'
-                class="flex-1 bg-gray-200 p-4 font-bold text-gray-700"
+                class="flex-1 bg-gray-200 p-4 font-light text-gray-700 tracking-widest"
                 :class="{ 'bg-teal-200 text-white' : currentTab == 4 }"
         >
           Bagels
         </button>
         <button @click='selectTab(5)'
-                class="flex-1 bg-gray-200 p-4 font-bold text-gray-700"
+                class="flex-1 bg-gray-200 p-4 font-light text-gray-700 tracking-widest"
                 :class="{ 'bg-teal-200 text-white' : currentTab == 5 }"
         >
           All
