019fb891581021d600a1aecb04e27c285a3884e2
Zach Holcomb <zachholcombmusic@gmail.com>
2020-10-28 17:44:17 -0600

    Updates styling on home splash and nav bar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -2,19 +2,64 @@
   <header class="pt-8 pb-12">
     <nav 
       :class="{ 'scrolled': !view.atTopOfPage }"
-      class="fixed flex w-full bg-white border-b items-center justify-between flex-wrap p-5 m-auto top-0 animated"
+      class="fixed flex w-full bg-white border-b items-center justify-between flex-wrap p-8 m-auto top-0 animated"
     >
         <div>
-          <router-link class="link-gray px-2 uppercase tracking-widest" to="/">Good Bread</router-link>
+          <router-link
+            v-on:click.native='selectPage(1)'
+            :class="{ 'selected' : currentPage == 1 }"
+            class="link-gray px-2 uppercase tracking-widest underline" 
+            to="/"
+          >
+            Good Bread
+          </router-link>
         </div>
         <div class="flex justify-center">
-          <router-link class="link-gray px-4" to="/shop">Order Online</router-link>
-          <router-link class="link-gray px-4" to="/dashboard" v-if="signedIn">Dashboard</router-link>
+          <router-link
+            v-on:click.native='selectPage(2)'
+            :class="{ 'selected' : currentPage == 2 }"
+            class="link-gray px-4" 
+            to="/shop"
+          >
+            Order Online
+          </router-link>
+          <router-link 
+            v-on:click.native='selectPage(3)'
+            :class="{ 'selected' : currentPage == 3 }"
+            class="link-gray px-4" 
+            to="/dashboard" 
+            v-if="signedIn"
+          >
+            Dashboard
+          </router-link>
         </div>
         <div class="flex justify-end">
-          <router-link class="link-gray px-4" to="/cart">Cart</router-link>
-          <router-link class="link-gray px-4" to="/login" v-if="!signedIn">Login</router-link>
-          <router-link class="link-gray px-4" to="/register" v-if="!signedIn">Register</router-link>
+          <router-link
+            v-on:click.native='selectPage(4)'
+            :class="{ 'selected' : currentPage == 4 }"
+            class="link-gray px-4" 
+            to="/cart"
+          >
+            Cart
+          </router-link>
+          <router-link
+            v-on:click.native='selectPage(5)'
+            :class="{ 'selected' : currentPage == 5 }"
+            class="link-gray px-4" 
+            to="/login" 
+            v-if="!signedIn"
+          >
+            Login
+          </router-link>
+          <router-link
+            v-on:click.native='selectPage(6)'
+            :class="{ 'selected' : currentPage == 6 }"
+            class="link-gray px-4" 
+            to="/register" 
+            v-if="!signedIn"
+          >
+            Register
+          </router-link>
           <a href="#" @click.prevent="signOut" class="link-gray px-2" v-if="signedIn">Log out</a>
         </div>
     </nav>
@@ -30,7 +75,8 @@ export default {
     return {
       view: {
         atTopOfPage: true
-      }
+      },
+      currentPage: 1
     }
   },
   beforeMount () {
@@ -41,6 +87,7 @@ export default {
       this.$store
         .dispatch(LOGOUT)
         .then(() => this.$router.replace('/'))
+        .then(() => this.currentPage = 1)
     },
     handleScroll () {
       if(window.pageYOffset > 0) {
@@ -48,6 +95,10 @@ export default {
       } else {
         if(!this.view.atTopOfPage) this.view.atTopOfPage = true
       }
+    },
+    selectPage (selectedPage) {
+      console.log(selectedPage)
+      this.currentPage = selectedPage
     }
   },
   computed: {
