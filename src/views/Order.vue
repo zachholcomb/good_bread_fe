a9ef98efe4902111d7c34378f3fe80a41dd4eb6b
Zach Holcomb <zachholcombmusic@gmail.com>
2020-10-28 17:45:01 -0600

    Adds order page to display individual orders

--- src/views/Order.vue
+++ src/views/Order.vue
@@ -1,14 +1,12 @@
 <template>
-  <div class="text-gray-700 max-w-4xl m-auto pt-8">
+  <div class="text-gray-700 max-w-4xl m-auto pt-4">
     <h3 class="text-4xl font-thin text-center pt-8 px-8 tracking-wider">
       Order # {{ order.id }}
     </h3> 
     <div class="text-2xl pt-2 pb-6 px-20 text-center">
+      <h3 class="font-thin pb-2">Placed on {{order.attributes.delivery_date}}</h3>
       <h3 class="font-thin">
-        Status:
-      </h3>
-      <h3 class="text-green-600 justify-self-center font-thin inline-block">
-        {{ order.attributes.status }}
+        Status: {{ order.attributes.status }}
       </h3>
     </div>
     <div class="px-12 grid grid-cols-3 max-w-4xl m-auto justify-items-center">
@@ -16,7 +14,7 @@
       <h3 class="text-gray-700 tracking-wider font-thin text-lg">Price</h3>
       <h3 class="text-gray-700 tracking-wider font-thin text-lg">Amount</h3>
     </div>
-    <div v-for="item in orderItems" v-bind:key="item.id">
+    <div v-for="item in orderItems.included" v-bind:key="item.id">
       <div class="max-w-4xl">
         <div class="pb-2">
           <ItemIcon v-bind:item="item"></ItemIcon>
@@ -25,7 +23,7 @@
     </div>
     <div class="pb-20">
       <h3 class="text-4xl text-gray-700 font-thin text-center pt-8 px-8 tracking-wider">
-        Total: 
+        Total: {{ convertPrice() }}
       </h3> 
     </div>
   </div>
@@ -51,6 +49,12 @@ export default {
   },
   components: {
     ItemIcon
+  }, 
+  methods: {
+    convertPrice () {
+      const price = this.order.attributes.total / 100
+      return `$ ${price.toFixed(2)}`
+    }
   }
 }
 </script>
