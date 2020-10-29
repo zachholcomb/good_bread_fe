124ae2e3b98518697f9fc8a986ce758f6f2cbbbe
Zach Holcomb <zachholcombmusic@gmail.com>
2020-10-29 11:13:32 -0600

    Adjusts text in user dashboard

--- src/views/UserDashboard.vue
+++ src/views/UserDashboard.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="m-auto py-10 bread-bg">
-    <h3 class=" text-6xl text-gray-400 mb-4 text-center tracking-widest uppercase font-thin">Welcome Back</h3>
+    <h3 class=" text-6xl text-white mb-4 text-center tracking-widest uppercase font-thin">Welcome Back</h3>
     <div class="pb-6">
       <ProfileStats></ProfileStats>
     </div>
@@ -26,8 +26,3 @@ export default {
   }
 }
 </script>
-<style scoped>
-  .bg-dashboard {
-    @apply bg-green-100
-  }
-</style>
