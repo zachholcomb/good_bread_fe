393420abca3ea2da98f6837f1a987882a77e8cd2
Zach Holcomb <zachholcombmusic@gmail.com>
2020-10-29 11:11:56 -0600

    Updates hom splash padding

--- src/components/HomeSplash.vue
+++ src/components/HomeSplash.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="py-24 bread-bg">
+  <div class="py-32 bread-bg">
     <div class="py-20 max-w-4xl m-auto">
       <h2 class="text-green-600 text-6xl text-center py-4 text-holder font-thin">Bakery Counter Now Open!</h2>
       <p class="text-green-600 text-2xl text-center py-3 text-holder">1515 Madison Street | Denver, CO | 80206</p>
-      <p class="text-green-600 text-2xl text-center pb-4 text-holder">Open 8am - Sold Out | Friday - Sunday</p>
+      <p class="text-green-600 text-2xl text-center pb-8 text-holder">Open 8am - Sold Out | Friday - Sunday</p>
     </div>
   </div>
 </template>
