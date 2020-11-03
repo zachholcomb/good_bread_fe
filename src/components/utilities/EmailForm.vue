f4f277ea30774d739370ea294f3aa00e6f342f99
Zach Holcomb <zachholcombmusic@gmail.com>
2020-11-03 00:02:32 -0700

    Changes site buttons to green

--- src/components/utilities/EmailForm.vue
+++ src/components/utilities/EmailForm.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="text-gray-700 w-full">
-    <form class="flex items-center border-b border-teal-500 py-2" @submit.prevent="newsLetter">
+    <form class="flex items-center border-b border-green-500 py-2" @submit.prevent="newsLetter">
       <input type="email" v-model="newsLetter" class="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" id="newsletter" placeholder="Subscribe to newsletter">
-      <button type="submit" class="flex-shrink-0 border-transparent border-4 bg-teal-500 hover:bg-teal-800 text-sm py-1 px-2 rounded text-white">Subscribe</button>
+      <button type="submit" class="flex-shrink-0 border-transparent border-4 bg-green-500 hover:bg-green-800 text-sm py-1 px-2 rounded text-white">Subscribe</button>
     </form>
   </div>
 </template>
