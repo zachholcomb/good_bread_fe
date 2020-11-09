a91252f7a3da72b82253fdd70a6ccd2e2f1a539e
Zach Holcomb <zachholcombmusic@gmail.com>
2020-11-09 14:54:44 -0700

    Updates css on homescreen

--- src/components/homepage/HomeSplash.vue
+++ src/components/homepage/HomeSplash.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="py-48 bread-bg">
-    <div class="py-12 rounded-lg shadow shadow-2xl max-w-4xl text-holder  m-auto">
-      <h2 class="text-green-600 text-6xl text-center py-4 font-thin">Bakery Counter Now Open!</h2>
-      <p class="text-green-600 text-2xl text-center py-3 ">1515 Madison Street | Denver, CO | 80206</p>
-      <p class="text-green-600 text-2xl text-center pb-8 ">Open 8am - Sold Out | Friday - Sunday</p>
+  <div class="home-screen">
+    <div class="home-screen-text-box">
+      <h2 class="home-screen-header">Bakery Counter Now Open!</h2>
+      <p class="home-screen-secondary-header">1515 Madison Street | Denver, CO | 80206</p>
+      <p class="home-screen-secondary-header pb-8">Open 8am - Sold Out | Friday - Sunday</p>
     </div>
   </div>
 </template>
