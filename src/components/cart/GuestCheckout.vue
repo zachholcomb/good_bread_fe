a95ba52cb4249b5724e5bf7dee16e100d5656190
Zach Holcomb <zachholcombmusic@gmail.com>
2020-11-20 10:45:15 -0700

    Updates checkout styling and guest register form

--- src/components/cart/GuestCheckout.vue
+++ src/components/cart/GuestCheckout.vue
@@ -2,7 +2,8 @@
   <div class="max-w-lg m-auto my-8 py-20 px-10">
     <h3 class="text-2xl mb-6 text-gray-700 tracking-widest">Guest Checkout</h3>
     <p class="pb-10 text-gray-700 pt-2">
-      We'll send you a confirmation email but you won't be able to sign in to our website. Register a new account if you don't already have one with us.
+      We'll send you a confirmation email but you won't be able to sign in to our website. 
+      Register a new account if you don't already have one with us to see your orders.
     </p>
     <button
       type="submit"
